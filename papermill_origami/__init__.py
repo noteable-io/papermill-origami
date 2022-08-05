5bb5b8a7deb208d215ad6d0ef51359544a971615
Rohit Sanjay <rohit.sanjay@noteable.io>
2022-08-04 18:44:53 -0700

    isort

--- papermill_origami/__init__.py
+++ papermill_origami/__init__.py
@@ -1,4 +1,4 @@
+from origami.client import ClientConfig, NoteableClient
+
 from .engine import NoteableEngine
 from .iorw import NoteableHandler
-
-from origami.client import NoteableClient, ClientConfig
