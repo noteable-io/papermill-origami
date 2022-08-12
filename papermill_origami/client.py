fbed59df8bed1c6d89550d8f650de3eb7ca86185
Rohit Sanjay <rohit.sanjay@noteable.io>
2022-08-12 10:39:56 -0700

    Add params to init

--- papermill_origami/client.py
+++ papermill_origami/client.py
@@ -1,4 +1,7 @@
+from typing import Optional, Union
+
 import origami.client
+from origami.client import ClientConfig, Token
 from papermill.engines import papermill_engines
 from papermill.iorw import papermill_io
 
@@ -9,7 +12,15 @@ from .iorw import NoteableHandler
 class NoteableClient(origami.client.NoteableClient):
     """Extended NoteableClient that registers NoteableEngine and NoteableHandler into papermill"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        api_token: Optional[Union[str, Token]] = None,
+        config: Optional[ClientConfig] = None,
+        follow_redirects=True,
+        **kwargs,
+    ):
+        super().__init__(
+            api_token=api_token, config=config, follow_redirects=follow_redirects, **kwargs
+        )
         papermill_io.register('noteable://', NoteableHandler(self))
         papermill_engines.register('noteable', NoteableEngine)
