a5f0077206f095614ce6110506f7851cc731a5d6
Elijah Wilson <eli@noteable.io>
2022-09-06 11:42:44 -0400

    setup basics

---
+++ papermill_origami/dagstermill/__init__.py
@@ -0,0 +1,6 @@
+"""This module combines Dagstermill with Noteable.
+
+To accomplish this a new `noteable-dagstermill` engine is created.
+This engine combines the DagstermillEngine with the NoteableEngine.
+To use this engine and module, ensure you install the `dagster` extra.
+"""
---
+++ papermill_origami/dagstermill/engine.py
@@ -0,0 +1,5 @@
+"""A Papermill engine that combines Dagstermill and Noteable."""
+
+
+class NoteableDagstermillEngine:
+    pass
