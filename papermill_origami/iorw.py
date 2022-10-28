acd4a2fe42f4a561a85303c090fc18dad8eac51b
Rohit Sanjay <rohit.sanjay@noteable.io>
2022-10-28 11:46:30 -0700

    remove unused import

--- papermill_origami/iorw.py
+++ papermill_origami/iorw.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 import httpx
 import structlog
 from jupyter_client.utils import run_sync
-from origami.client import ClientConfig, NoteableClient
+from origami.client import NoteableClient
 from origami.types.files import FileVersion
 
 from papermill_origami.util import parse_noteable_file_id
