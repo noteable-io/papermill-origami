c85dd075ca17500a17363464622578e8485ca4c2
Rohit Sanjay <rohit.sanjay@noteable.io>
2022-11-07 15:51:09 -0800

    remove comments

--- papermill_origami/noteable_airflow/operator.py
+++ papermill_origami/noteable_airflow/operator.py
@@ -1,14 +1,9 @@
 import papermill as pm
 from airflow.models.baseoperator import BaseOperator
 
-# TODO: should we use papermill-origami to register an entrypoint for the operator?
-#       https://airflow.apache.org/docs/apache-airflow-providers/index.html#creating-your-own-providers
-
 
 class NoteablePapermillOperator(BaseOperator):
     """Executes a notebook via papermill on Noteable and returns the output notebook object."""
-
-    # TODO: should we support lineage and add inlets/outlets?
 
     def __init__(
         self, notebook_path: str, parameters: dict, output_path: str = None, **kwargs
@@ -19,7 +14,6 @@ class NoteablePapermillOperator(BaseOperator):
         self.parameters = parameters
 
     def execute(self, context):
-        # TODO: Construct job instance metadata from context and pass to papermill
         return pm.execute_notebook(
             self.notebook_path,
             self.output_path,
