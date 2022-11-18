496c863b05c2fec5f2893e8918f03cfde2cc825a
Rohit Sanjay <rohit.sanjay@noteable.io>
2022-11-18 14:17:26 -0800

    Accept customer job definition reference id if provided

--- papermill_origami/noteable_dagstermill/engine.py
+++ papermill_origami/noteable_dagstermill/engine.py
@@ -5,6 +5,7 @@ from ..engine import NoteableEngine
 class NoteableDagstermillEngine(NoteableEngine):
     async def execute(self, **kwargs):
         job_metadata = kwargs.setdefault("job_metadata", {})
-        job_metadata["orchestrator_id"] = "dagster"
-        job_metadata["orchestrator_name"] = "Dagster"
+        job_metadata["customer_job_definition_reference"] = {}
+        job_metadata["customer_job_definition_reference"]["orchestrator_id"] = "dagster"
+        job_metadata["customer_job_definition_reference"]["orchestrator_name"] = "Dagster"
         return await super().execute(**kwargs)
